707bf1be2a951b85fbb9ba9375ae61b70b743ab9
Veasey, Patrick <patrick.veasey@hmhco.com>
2019-01-09 12:55:02 -0600

    allow for port to pass via LDAP_PORT and LDAPS_PORT env variables

--- image/service/slapd/process.sh
+++ image/service/slapd/process.sh
@@ -9,4 +9,17 @@ log-helper level eq trace && set -x
 # see https://github.com/docker/docker/issues/8231
 ulimit -n $LDAP_NOFILE
 
-exec /usr/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u openldap -g openldap -d $LDAP_LOG_LEVEL
+if [ -z "$LDAP_PORT" ]; then
+    LDAP_CONNETION_STRING=$HOSTNAME
+else
+    LDAP_CONNETION_STRING=$HOSTNAME:$LDAP_PORT
+fi
+
+if [ -z "$LDAPS_PORT" ]; then
+    LDAPS_CONNETION_STRING=$HOSTNAME
+else
+    LDAPS_CONNETION_STRING=$HOSTNAME:$LDAPS_PORT
+fi
+
+
+exec /usr/sbin/slapd -h "ldap://$LDAP_CONNETION_STRING ldaps://$LDAPS_CONNETION_STRING ldapi:///" -u openldap -g openldap -d $LDAP_LOG_LEVEL
